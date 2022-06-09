0b8ca015655d9211bb4ed0f98a99764d21cb3cfb
Jeonghyeon-Ryu <vxxv123@naver.com>
2022-06-09 17:03:09 +0900

    22.06.09 Javascript Commit

---
+++ Javascript-DOM/javascript-dom.js
@@ -0,0 +1,49 @@
+window.addEventListener("load", function() {
+    var txtX = document.getElementById("txt-x");
+    var txtY = document.getElementById("txt-y");
+    var btnEqual = document.getElementById("btn-equal");
+    var txtResult = document.getElementById("txt-result");
+
+    btnEqual.onclick = function() {
+        txtResult.value = parseInt(txtX.value)+parseInt(txtY.value);
+    };
+});
+
+window.addEventListener("load", function() {
+    var section2 = document.getElementById("section2");
+    var inputItem = section2.getElementsByTagName("input");
+
+    inputItem[2].onclick = function() {
+        inputItem[3].value = parseInt(inputItem[0].value)+parseInt(inputItem[1].value);
+    };
+});
+// Ex3 : Slector 계산기 프로그램
+window.addEventListener("load", function() {
+    var section3 = document.getElementById("section3");
+    // var inputItem = section3.getElementsByTagName("input");
+    var txtX = section3.querySelector("input[name='x']");
+    var txtY = section3.querySelector(".txt-y");
+    var btnEqual = section3.querySelector(".btn-equal");
+    var txtResult = section3.querySelector("input[name='result']");
+
+    btnEqual.onclick = function() {
+        txtResult.value = parseInt(txtX.value)+parseInt(txtY.value);
+    };
+});
+
+// Ex4 : childNodes 를 이용한 노드선택
+window.addEventListener("load", function() {
+    var section4 = this.document.querySelector("#section4");
+    var box = section4.querySelector(".box");
+    // childNodes : 공백도 노드 취급
+    // var input1 = box.childNodes[0];
+    // var input2 = box.childNodes[1];
+    // children : 태그만 취급
+    var input1 = box.children[0];
+    var input2 = box.children[1];
+1  
+    input1.value="hello";
+    input2.value="okey";
+
+
+});

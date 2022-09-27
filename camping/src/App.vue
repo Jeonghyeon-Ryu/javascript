8ff3fe28531bb534caa8c471af3cd7826a51bbfe
Jeonghyeon-Ryu <vxxv123@naver.com>
2022-09-28 05:33:42 +0900

    22.09.28

---
+++ camping/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <HeaderNav></HeaderNav>
+    <MainSwiper></MainSwiper>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HeaderNav from './components/HeaderNav.vue'
+import MainSwiper from './components/MainSwiper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HeaderNav,
+    MainSwiper
+}
+}
+</script>
+
+<style>
+  * { 
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    font-style: none;
+    box-sizing: border-box;
+  }
+  body {
+    height:2000px;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
---
+++ camping/src/components/HeaderNav.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <div class="header-top-container">
+      <div class="header-title">
+        <img src="../assets/img/camp-icon.png" class="header-logo">
+        <span class="header-name">캠핑 가자 !</span> 
+      </div>
+      <div class="header-top-button">
+        <div><img src="../assets/img/login-30.png" alt=""></div>
+        <div><img src="../assets/img/chat-30.png" alt=""></div>
+        <div><img src="../assets/img/bell-30.png" alt=""></div>
+        <div><img src="../assets/img/menu-30.png" alt=""></div>
+      </div>
+    </div>
+    <div class="header-middle-button" >
+      <div>캠핑장</div> 
+      <div>캠핑장</div> 
+      <div>캠핑장</div> 
+      <div>캠핑장</div> 
+    </div>
+  </header>
+</template>
+
+<script>
+    export default {
+      name: 'HeaderNav',
+        props:['category'],
+        
+    }
+</script>
+
+<style scoped src="../assets/css/header.css"></style>

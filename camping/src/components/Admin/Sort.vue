3d50c471c8477f875f8b95b1c3138114787f0d45
Jeonghyeon-Ryu <vxxv123@naver.com>
2022-10-04 01:38:26 +0900

    22.10.04

---
+++ camping/src/components/Admin/Sort.vue
@@ -0,0 +1,44 @@
+<template>
+    <div @click="clickSort" class="sort-container">
+        <img v-bind:src="source"/>
+    </div>
+</template>
+
+<script>
+    import asc from '@/assets/img/Table/asc.png'
+    import desc from '@/assets/img/Table/desc.png'
+
+    export default {
+        props : ['column'],
+        data: function () {
+            return {
+                type : 'asc'
+            }
+        },
+        methods: {
+            clickSort : function () {
+                if(this.type == 'asc') {
+                    this.type = 'desc';
+                } else {
+                    this.type = 'asc';
+                }
+            }
+        },
+        computed: {
+            source : function() {
+                if(this.type == 'asc'){
+                    return desc;
+                } else {
+                    return asc;
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .sort-container {
+        width: 40px;
+        height: 40px;
+    }
+</style>

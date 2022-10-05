541d23b4fa6e6eb4e8fd9e039a7d5fe41bd3b7b8
띠용 <103794752+Jeonghyeon-Ryu@users.noreply.github.com>
2022-10-05 17:46:50 +0900

    22.10.05

--- camping/src/components/Admin/UserManage.vue
+++ camping/src/components/Admin/UserManage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="user-manage-container">
         <div class="user-manage-title">
-
+            <h2>유저 관리 페이지</h2>
         </div>
         <div class="user-manage-table">
             <UserTable :userData="userData" :perPage="perPage"></UserTable>
@@ -10,31 +10,37 @@
 </template>
 <!-- -------------------------------------- -->
 <script>
-    import UserTable from './UserTable.vue';
-    import users from './users';
-    export default {
-        data : function () {
-            return {
-                userData : users,
-                perPage : 20
-            }
-        },  
-        components : {
-            UserTable
+import UserTable from './UserTable.vue';
+import users from './users';
+export default {
+    data: function () {
+        return {
+            userData: users,
+            perPage: 20
         }
+    },
+    components: {
+        UserTable
     }
+}
 </script>
 <!-- -------------------------------------- -->
 <style scoped>
-    .user-manage-container{
-        display:flex;
-        justify-content: center;
-        margin-top : 150px;
-        width:100%;
-        height: calc(100vw - 150px)
-    }
-    .user-manage-table {
-        width: 90%;
-        height: 100%;
-    }
+.user-manage-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 150px;
+    width: 100%;
+    height: calc(100vh - 150px)
+}
+
+.user-manage-container h2 {
+    padding: 20px;
+}
+
+.user-manage-table {
+    width: 90%;
+    height: 100%;
+}
 </style>
